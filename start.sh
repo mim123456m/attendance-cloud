be04d79ecd472510a3eb530e1e0963cc906fcc67
mim123456m <97371774+mim123456m@users.noreply.github.com>
2026-01-06 23:03:44 +0700

    Update and rename start.sh to 1

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# ถ้า Railway ส่ง PORT มา
-if [ -n "$PORT" ]; then
-  echo "Listen $PORT" > /etc/apache2/ports.conf
-  sed -i "s/:80/:$PORT/g" /etc/apache2/sites-enabled/000-default.conf
-fi
-
-exec apache2-foreground
